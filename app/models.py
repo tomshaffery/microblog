51de31c4b59be0d959bd42421809f2f3fd98faf9
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-03-21 09:58:18 -0400

    Syntax edit

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class User(db.Model):
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
-    timestamp = so.Mapped[datetime] = so.mapped_column(
+    timestamp: so.Mapped[datetime] = so.mapped_column(
         index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
                                                index=True)
