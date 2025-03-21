556e9c7d37477e16bb2d88bd9e34e48e044392c3
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-03-21 10:02:12 -0400

    Posts added to User in Models

--- app/models.py
+++ app/models.py
@@ -10,6 +10,10 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(12), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
+    
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
     
