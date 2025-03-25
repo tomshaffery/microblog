963453898fa97e81ce920e0ccfc94aab40a1bff5
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-03-25 18:32:15 -0400

    User loader and login authentication added

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
+import sqlalchemy as sa
 from app import app
+from app import db
+from app.models import User
 from app.forms import LoginForm
 
 @app.route('/')
@@ -20,9 +24,15 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
