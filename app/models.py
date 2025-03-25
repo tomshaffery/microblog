8ab7c1c0bd66c52c82328fe28fc296bae8966d46
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-03-24 23:07:15 -0400

    Flask-Login added to models

--- app/models.py
+++ app/models.py
@@ -4,8 +4,14 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from app import login
+from flask_login import UserMixin
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return db.sesion.get(User, int(id))
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(12), index=True, unique=True)
