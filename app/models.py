08efa5b9bfe9fffe35fd43ea23cbe4a849291185
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-03-29 15:17:33 -0400

    User avatars

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ import sqlalchemy.orm as so
 from app import db
 from app import login
 from flask_login import UserMixin
+from hashlib import md5
 
 @login.user_loader
 def load_user(id):
@@ -29,6 +30,10 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
